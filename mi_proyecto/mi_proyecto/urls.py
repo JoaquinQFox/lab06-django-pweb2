ea315a8cc820ed36233788a76626e17ddc3017e8
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-20 15:04:05 -0500

    Se configuraron las url en el archivo urls.py

--- mi_proyecto/mi_proyecto/urls.py
+++ mi_proyecto/mi_proyecto/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from . import views
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('crear/', views.crear_venta, name='crear_venta'),
+    path('ventas/', include('tienda.urls')),
+	path('', views.lista_ventas, name='lista_venta'),
 ]
